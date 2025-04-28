255ebd6e51ebb3c1f581a18e7fffb0dc6a683d92
Ihász Krisztina <ihaszkrisztina@Ihasz-MacBook-Air.local>
2025-04-29 00:25:06 +0200

    Leírás a változásról

--- kavezok/settings.py
+++ kavezok/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-fimr!5c5$w66a@u^z5)(m^xq!^^btdw460xok9*vp-3b)_-18!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ALLOWED_HOSTS = ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'kavekvilag.onrender.com']
 
 
 # Application definition
