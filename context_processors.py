194a6c1e83b043fbd1772f05f2cadb23b0fc8def
Ihász Krisztina <ihaszkrisztina@Ihasz-MacBook-Air.local>
2025-04-29 21:11:56 +0200

    Add initial migrations

--- context_processors.py
+++ context_processors.py
@@ -1,5 +1,5 @@
 # context_processors.py
 def user_context(request):
     return {
-        'user': request.user  # A bejelentkezett felhasználó elérhető lesz, vagy AnonymousUser, ha nincs bejelentkezve
+        'user': request.user  # A bejelentkezett felhasználó elérhető lesz, vagy AnonymousUs
     }
